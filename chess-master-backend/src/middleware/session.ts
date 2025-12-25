b49cf74803b154734fd8577bba45f23fce807a5c
pooyaostvoar <pooya.ostovar@gmail.com>
2025-12-25 23:29:57 +0100

    remove redundant log

--- chess-master-backend/src/middleware/session.ts
+++ chess-master-backend/src/middleware/session.ts
@@ -3,7 +3,6 @@ import RedisStore from "connect-redis";
 import { createClient } from "redis";
 import { readSecret } from "../utils/secret";
 
-console.log("DEBUG: REDIS_URL in session.ts:", process.env.REDIS_URL, "NODE_ENV:", process.env.NODE_ENV);
 const redisClient = createClient({
   url:
     readSecret("/run/secrets/redis_url") ||
